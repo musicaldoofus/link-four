ae559b30b442a00c1a2137bba6bb75b4c70a306c
musicaldoofus <burns.trainingdesign@gmail.com>
2019-09-13 11:59:00 -0700

    Basic updates to GameBoard, Column, and Token styles to look more like original game

--- src/components/HUD/index.js
+++ src/components/HUD/index.js
@@ -4,6 +4,11 @@ import './HUD.css';
 const HUD = ({scores, handleRefresh}) => {
     return (
         <div className="heads-up-display">
+            <div id="game-title">
+                <div>
+                    <h1>Link Four</h1>
+                </div>
+            </div>
             <div id="user-score">
                 <div>{scores.user}</div>
                 <div>User</div>
